f68eeb3a432716106791a8cf3f6f153988e97516
lenor21 <dejesusronel29@gmail.com>
2023-12-11 22:12:35 +0800

    working on hero section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,25 @@ const Home = () => {
     <div className='p-home'>
       <section className='p-home__hero'>
         <div className='container'>
-          <div className='p-home__hero__inner'>hello</div>
+          <div className='p-home__hero__inner'>
+            <div className='p-home__hero__inner__details'>
+              <p className='p-home__hero__inner__details__sml'>HELLO WORLD, WELCOME TO MY SITE</p>
+              <h1 className='p-home__hero__inner__details__head'>
+                I’m Ronel De Jesus <br /> A Software & Web Developer
+              </h1>
+              <div className='p-home__hero__inner__details__btn-cont'>
+                <a href='#' className='c-button'>
+                  Download CV
+                </a>
+                <a href='#' className='c-button'>
+                  Contact Me
+                </a>
+              </div>
+            </div>
+            <div className='p-home__hero__inner__img-cont'>
+              <img src='../public/images/pages/home/pc/hero.svg' alt='hero-img' className='p-home__hero__inner__img-cont__img' />
+            </div>
+          </div>
         </div>
       </section>
     </div>
