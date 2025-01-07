ea226fbbf9ed23ec6a3673e67379058204f63b73
lhc-dejesusron <r.dejesus@lionheart.asia>
2025-01-07 15:14:26 +0800

    added lenis smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RootLayout from './pages/root-layout/RootLayout';
 import Home from './pages/home/Home';
+import Lenis from 'lenis';
+import 'lenis/dist/lenis.css';
+
+// Initialize Lenis
+const lenis = new Lenis({
+  duration: 3,
+});
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 const App = () => {
   return (
